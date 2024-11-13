4aab7e6733ec4b5862492b511f112376c2168e59
Josh <josh@askhomer.ai>
2024-11-13 09:49:10 +0800

    -

--- dist/src/services/knowledgebaseService.js
+++ dist/src/services/knowledgebaseService.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.KnowledgeBaseService = void 0;
 const storage_blob_1 = require("@azure/storage-blob");
 const identity_1 = require("@azure/identity");
-const azure_blob_storage_container_1 = require("@langchain/community/document_loaders/web/azure_blob_storage_container");
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 class KnowledgeBaseService {
@@ -37,31 +36,6 @@ class KnowledgeBaseService {
             return '0 Byte';
         const i = Math.floor(Math.log(bytes) / Math.log(1024));
         return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + ' ' + sizes[i];
-    }
-    async loadFiles(containerName) {
-        try {
-            console.log("Loading files...");
-            const credential = new identity_1.DefaultAzureCredential();
-            const loader = new azure_blob_storage_container_1.AzureBlobStorageContainerLoader({
-                azureConfig: {
-                    connectionString: "DefaultEndpointsProtocol=https;AccountName=askesterstorage;AccountKey=/m5nHZ8QaklLsYcOu1JWjMQUEfvgRa/lHLC/e3sB+ryZ+bRPBTeEzSNkV+C8xQeDppbzl1Zl8kaA+AStro3ymQ==;EndpointSuffix=core.windows.net",
-                    container: containerName
-                },
-                unstructuredConfig: {
-                    apiUrl: "https://api.unstructuredapp.io/general/v0/general",
-                    apiKey: "L8wioFiZ2NWbvgn4usTbz8ZWjjqyoB"
-                }
-            });
-            console.log("...");
-            console.log("...");
-            const docs = await loader.load();
-            console.log("Loaded");
-            console.log(docs);
-        }
-        catch (error) {
-            console.error('Error loading files:', error);
-            throw error;
-        }
     }
 }
 exports.KnowledgeBaseService = KnowledgeBaseService;
